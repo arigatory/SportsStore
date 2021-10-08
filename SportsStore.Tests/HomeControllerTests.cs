a119502c06f28389c0a02fff7778be518ce45481
Ivan MSI <arigatory@gmail.com>
2021-10-09 01:18:20 +0300

    several views

--- SportsStore.Tests/HomeControllerTests.cs
+++ SportsStore.Tests/HomeControllerTests.cs
@@ -30,5 +30,27 @@ namespace SportsStore.Tests
             Assert.Equal("P1", productsArray[0].Name);
             Assert.Equal("P2", productsArray[1].Name);
         }
+
+        [Fact]
+        public void Can_Paginate()
+        {
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+                new Product { ProductId=1, Name="P1"},
+                new Product { ProductId=2, Name="P2"},
+                new Product { ProductId=3, Name="P3"},
+                new Product { ProductId=4, Name="P4"},
+                new Product { ProductId=5, Name="P5"}
+            }).AsQueryable<Product>());
+            HomeController controller = new HomeController(mock.Object);
+            controller.PageSize = 3;
+
+            IEnumerable<Product> result = (controller.Index(2) as ViewResult).ViewData.Model as IEnumerable<Product>;
+
+            Product[] prodArray = result.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P4", prodArray[0].Name);
+            Assert.Equal("P5", prodArray[1].Name);
+        }
     }
 }
