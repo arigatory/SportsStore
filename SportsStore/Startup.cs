e6d893cc3072e2cf870e81ec524a7cfbf5def11e
Ivan MSI <arigatory@gmail.com>
2021-10-09 00:20:01 +0300

    one view of products

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -5,18 +5,33 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SportsStore.Models;
 
 namespace SportsStore
 {
     public class Startup
     {
+        private IConfiguration Configuraion { get; set; }
+
+        public Startup(IConfiguration config)
+        {
+            Configuraion = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<StoreDbContext>(opt =>
+            {
+                opt.UseSqlServer(
+                    Configuraion["ConnectionStrings:SportsStoreConnection"]);
+            });
+            services.AddScoped<IStoreRepository, EFStoreRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,6 +45,7 @@ namespace SportsStore
             {
                 endnpoints.MapDefaultControllerRoute();
             });
+            SeedData.EnsurePopulated(app);
         }
     }
 }
